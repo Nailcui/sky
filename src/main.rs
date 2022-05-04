99ee83ea218274f542e17be9dc0623e668fb7809
nailcui <nailcui@foxmail.com>
2022-05-04 22:20:44 +0800

    file server with rocket, could get file by filename

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use std::path::{Path, PathBuf};
+
 #[get("/")]
 fn index() -> &'static str {
     "ok!"
 }
 
 #[get("/obj/<filename>")]
-fn get_file(filename: &str) -> String {
-    format!("filename: {}", filename)
+async fn get_file(filename: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = Path::new("/tmp/sky/").join(filename);
+    NamedFile::open(&path)
+        .await
+        .map_err(|e| NotFound(e.to_string()))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index, get_file])
+    rocket::build().mount("/", routes![index, get_file])
 }
